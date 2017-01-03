ee1ef402b8e9056b2267bbeaaac1ee5dd861733c
Christian Wardell <cwardell@cubistmediagroup.com>
2017-01-03 09:58:41 -0500

    apparently webpack config has changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const webpack = require('webpack'),
     path = require('path'),
     ExtractTextPlugin = require("extract-text-webpack-plugin");
-    //webpackDevMiddleware = require("webpack-dev-middleware");
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+//webpackDevMiddleware = require("webpack-dev-middleware");
 
 const config = {
-    entry:{bundle:[
-        //'dev':path.resolve('webpack-dev-server.js'),
-        path.resolve(__dirname,'precompile/build.js'),
-        'webpack/hot/dev-server'
-        //'webpack-hot-middleware/client'
+    entry: {
+        bundle: [
+            //'dev':path.resolve('webpack-dev-server.js'),
+            path.resolve(__dirname, 'precompile/build.js'),
+            'webpack/hot/dev-server'
+            //'webpack-hot-middleware/client'
 
-    ]},
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'lib'),
         filename: '[name].js',
-        publicPath: "http://localhost:3000/"
+        publicPath: "http://localhost/"
     },
     resolve: {
         modulesDirectories: ["web_modules", "node_modules", "bower_components", "js"]
@@ -39,7 +42,25 @@ const config = {
         // }),
         new webpack.HotModuleReplacementPlugin(),
         // activates HMR
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new BrowserSyncPlugin(
+            // BrowserSync options 
+            {
+                // browse to http://localhost:3000/ during development 
+                host: 'localhost',
+                port: 3100,
+                // proxy the Webpack Dev Server endpoint 
+                // (which should be serving on http://localhost:3100/) 
+                // through BrowserSync 
+                proxy: 'http://localhost:3000/'
+            },
+            // plugin options 
+            {
+                // prevent BrowserSync from reloading the page 
+                // and let Webpack Dev Server take care of this 
+                reload: false
+            }
+        )
 
     ],
 };
